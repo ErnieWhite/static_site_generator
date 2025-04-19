7f8d5d651907111e5a8ffc029ba1b27628b5e3a0
ErnieWhite <ernie.white.68@gmail.com>
2025-04-19 17:46:59 -0500

    Catching up

--- src/main.py
+++ src/main.py
@@ -22,6 +22,30 @@ def text_node_to_html_node(text_node:TextNode):
         
     raise ValueError(f"{text_node.text_type} is not implemented")
 
+def split_nodes_delimiter(old_nodes: TextNode, delimiter, text_type):
+
+    new_nodes = []
+    for old_node in old_nodes:
+
+        # If an "old node" is not a TextType.TEXT type, just add it to the 
+        # new list as-is, we only attempt to split "text" type objects
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        # If a matching closing delimiter is not found, raise an InvalidSyntax
+        # exception
+        # **bold** this is bold -> 3
+        # this is **bold** -> 3
+        # is **bold** bold -> 3
+        # hello **bold** hi **bold** goobbye -> 5
+        # **bold this is not bold -> ["", "bold this is not bold"] -> 2
+        # **bold this **is** not bold -> ["", "bold this", "is", "not bold]"] -> 4
+        tokens = old_node.text.split(delimiter)
+        if len(tokens) % 2 != 1:
+            raise SyntaxError(f'Matching delimiter "{delimiter}" not found')
+
+    return new_nodes
+
 
 def main():
     textnode = TextNode("Anchor", TextType.LINK, "https://www.boot.dev")
--- src/test_main.py
+++ src/test_main.py
@@ -1,8 +1,13 @@
 import unittest
-from main import text_node_to_html_node
+from main import text_node_to_html_node, split_nodes_delimiter
 from textnode import TextNode, TextType
 
 class TestMain(unittest.TestCase):
+
+    def text_text_node_to_html_example(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+
 
     def test_text_node_to_html_text(self):
         text = "This is a text node"
@@ -35,3 +40,40 @@ class TestMain(unittest.TestCase):
         self.assertEqual(html_node.tag, "a")
         self.assertEqual(html_node.value, text)
         self.assertEqual(html_node.props["href"], url)
+
+    def text_text_node_to_html_example(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(
+           [ 
+               TextNode("This is text with a ", TextType.TEXT),
+               TextNode("code block", TextType.CODE),
+               TextNode(" word", TextType.TEXT),
+           ],
+           new_nodes
+        )
+
+    def test_split_nodes_delimiter(self):
+        node = TextNode("This is a bold text node", TextType.BOLD)
+        delimiter = "**"
+        split_node = split_nodes_delimiter([node], delimiter, TextType.BOLD)
+        self.assertEqual([node], split_node)
+
+    def test_split_nodes_delimiter_no_matching_delimiter(self):
+        node = TextNode("This has malformed **bold text", TextType.TEXT)
+        delimiter = "**"
+        with self.assertRaises(SyntaxError) as context:
+            split_node = split_nodes_delimiter([node], delimiter, TextType.BOLD)
+            self.assertEqual(str(context.exception), f'Matching delimiter {delimiter} not found')
+
+    def test_split_nodes_delimiter_start_bold(self):
+        node = TextNode("**bold** at the start", TextType.TEXT)
+        delimiter = "**"
+        split_nodes = split_nodes_delimiter([node], delimiter, TextType.BOLD)
+        self.assertEqual(
+            [
+                TextNode("bold", TextType.BOLD),
+                TextNode(" at the start", TextType.TEXT)
+            ],
+            split_nodes
+        )
