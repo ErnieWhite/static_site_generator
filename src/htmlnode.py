bd9fae26189a9d2335a66e38bbce5a3dcc3fe22a
ErnieWhite <ernie.white.68@gmail.com>
2025-04-20 06:40:02 -0500

    Removed type hinting

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,8 +1,5 @@
-from typing import Dict, Iterable
-
-
-class HTMLNode():
-    def __init__(self, tag=None, value=None, children=None, props:Dict[str,str]|None=None):
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
         self.value = value
 
