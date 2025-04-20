9f20313ddfc6bfaf7a2b78326bdb0c544665272a
ErnieWhite <ernie.white.68@gmail.com>
2025-04-20 07:31:43 -0500

    Replaced my code with boot.devs.  Did the same thing but much cleaner.

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,29 +1,22 @@
 from textnode import TextNode, TextType
-def split_nodes_delimiter(old_nodes: TextNode, delimiter, text_type):
 
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     for old_node in old_nodes:
-
-        # If an "old node" is not a TextType.TEXT type, just add it to the
-        # new list as-is, we only attempt to split "text" type objects
         if old_node.text_type != TextType.TEXT:
             new_nodes.append(old_node)
             continue
-        # If a matching closing delimiter is not found, raise an InvalidSyntax
-        # exception
-        # **bold** this is bold -> 3
-        # this is **bold** -> 3
-        # is **bold** bold -> 3
-        # hello **bold** hi **bold** goobbye -> 5
-        # **bold this is not bold -> ["", "bold this is not bold"] -> 2
-        # **bold this **is** not bold -> ["", "bold this", "is", "not bold]"] -> 4
-        tokens = old_node.text.split(delimiter)
-        if len(tokens) % 2 != 1:
-            raise SyntaxError(f'Matching delimiter "{delimiter}" not found')
-        new_nodes.extend(
-            [
-                TextNode(tokens[0], TextType.TEXT),
-                TextNode(tokens[1], text_type),
-                TextNode(tokens[2], TextType.TEXT)
-            ]
-        )
+        split_nodes = []
+        sections = old_node.text.split(delimiter)
+        if len(sections) % 2 == 0:
+            raise ValueError("invalid markdown, formatted section not closed")
+        for i in range(len(sections)):
+            if sections[i] == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(sections[i], TextType.TEXT))
+            else:
+                split_nodes.append(TextNode(sections[i], text_type))
+        new_nodes.extend(split_nodes)
+    return new_nodes
