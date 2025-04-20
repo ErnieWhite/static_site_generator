e0d2118a05ab8cf1a051cf4c0ba645270ac23b8e
ErnieWhite <ernie.white.68@gmail.com>
2025-04-20 07:33:14 -0500

    Changed textnode to node

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 from textnode import TextNode, TextType
 
 def main():
-    textnode = TextNode("Anchor", TextType.LINK, "https://www.boot.dev")
-    print(textnode)
+    node = TextNode("Anchor", TextType.LINK, "https://www.boot.dev")
+    print(node)
 
 if __name__ == "__main__":
     main()
