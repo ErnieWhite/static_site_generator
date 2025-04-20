17fc69f3dde403110958e7b0095555c0b3de56c6
ErnieWhite <ernie.white.68@gmail.com>
2025-04-20 07:13:08 -0500

    Moved all children of HTMLNode to htmlnode.py

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -17,7 +17,7 @@ class HTMLNode:
         raise NotImplementedError("to_html method not implemented")
 
     def props_to_html(self):
-        if not self.props:
+        if self.props is None:
             return ""
         props_string = "" 
         for k,v in self.props.items():
@@ -27,3 +27,48 @@ class HTMLNode:
     def __repr__(self):
         props = self.props_to_html()
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, props=None):
+        # no children are allowed so pass None to the parent
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        # if the leaf node has no value, it hsould raise a ValueError.  All leaf nodes must have a value.
+        if self.value == "" or self.value is None:
+            raise ValueError("All leaf nodes must have a value.")
+        # if there is no tag (e.g. it's None), the value should be returned as raw text.
+        if self.tag is None:
+            return self.value
+        # otherwise, it should render an HTML tag. For example, these leaf nodes:
+        #
+        # LeafNode("p", "This is a paragraph of text.").to_html()
+        # <p>This is a paragraph of text.</p>
+        #
+        # LeafNode("a", "Click me!", {"href": "https://www.google.com"}).to_html()
+        # <a href="https://www.google.com">Click me!</a>
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def children_to_html(self):
+        result_html = f"<{self.tag}{self.props_to_html()}>"
+        for child in self.children:
+            result_html += child.to_html()
+        result_html += f"</{self.tag}>"
+        return result_html
+
+    def to_html(self):
+        # if the object doesn't have a tag, raise a ValueError
+        if self.tag is None or self.tag == "":
+            raise ValueError("All parent nodes must have a tag")
+        # if children is missing, raise a ValueError
+        if self.children is None:
+            raise ValueError("All parent nodes must have children")
+        # otherwise, return a string representing the HTML tag of the node and its children recursively.
+        child_html = self.children_to_html()
+        return child_html
