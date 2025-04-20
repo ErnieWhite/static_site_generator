770ccdf603e6d9f0614d27e1df0299a8a5970c0c
ErnieWhite <ernie.white.68@gmail.com>
2025-04-20 07:19:27 -0500

    Moved text_node_to_html_node to textnode.py

--- src/main.py
+++ src/main.py
@@ -1,26 +1,6 @@
 from textnode import TextNode, TextType
 from leafnode import LeafNode
 
-def text_node_to_html_node(text_node:TextNode):
-    if text_node.text_type == TextType.TEXT:
-        return LeafNode(value=text_node.text)
-
-    if text_node.text_type == TextType.BOLD:
-        return LeafNode(tag="b", value=text_node.text)
-
-    if text_node.text_type == TextType.ITALIC:
-        return LeafNode("i", text_node.text)
-
-    if text_node.text_type == TextType.CODE:
-        return LeafNode("code", text_node.text)
-
-    if text_node.text_type == TextType.LINK:
-        return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
-
-    if text_node.text_type == TextType.IMAGE:
-        return LeafNode(tag="img", value="", props={"src":text_node.url, "alt": text_node.text})
-        
-    raise ValueError(f"{text_node.text_type} is not implemented")
 
 def split_nodes_delimiter(old_nodes: TextNode, delimiter, text_type):
 
--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,4 @@
+from htmlnode import LeafNode
 from enum import Enum
 
 class TextType(Enum):
@@ -20,3 +21,24 @@ class TextNode:
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
 
+
+def text_node_to_html_node(text_node: TextNode):
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(value=text_node.text)
+
+    if text_node.text_type == TextType.BOLD:
+        return LeafNode(tag="b", value=text_node.text)
+
+    if text_node.text_type == TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+
+    if text_node.text_type == TextType.CODE:
+        return LeafNode("code", text_node.text)
+
+    if text_node.text_type == TextType.LINK:
+        return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
+
+    if text_node.text_type == TextType.IMAGE:
+        return LeafNode(tag="img", value="", props={"src": text_node.url, "alt": text_node.text})
+
+    raise ValueError(f"{text_node.text_type} is not implemented")
